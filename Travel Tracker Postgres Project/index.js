ac3efa0f80b87cfb63bab1237f8dec9f6c630d2d
Aryan Tambe <aryan.tambe04@gmail.com>
2025-05-09 19:59:10 +0530

    Added the home page route, which colors countries I have visited i.e stored in my Postgres table

--- Travel Tracker Postgres Project/index.js
+++ Travel Tracker Postgres Project/index.js
@@ -1,14 +1,32 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "first",
+  password: "Aryan@2004",
+  port: 5432,
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  for (let i = 0; i < result.rows.length; i++) {
+    countries.push(result.rows[i].country_code);
+  }
+  console.log(result.rows);
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
 });
 
 app.listen(port, () => {
