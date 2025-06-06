881ff0909b471bd336add11145756368862943a3
Aryan Tambe <aryan.tambe04@gmail.com>
2025-06-07 00:16:49 +0530

    Added password checker for login

--- Authentication+Lv.1/index.js
+++ Authentication+Lv.1/index.js
@@ -31,17 +31,42 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
   let email = req.body.username;
   let password = req.body.password;
-  const result = await db.query(
-    "INSERT INTO users (email,password) VALUES($1,$2)",
-    [email, password]
-  ); //inserting the registered usernames in the database
-  console.log(result);
-  res.render("secrets.ejs"); //rendering the ejs file after successfull registration
+
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (checkResult.rowCount > 0) {
+      res.json("Email is already registered, Try logging in!");
+    } else {
+      const result = await db.query(
+        "INSERT INTO users (email,password) VALUES($1,$2)",
+        [email, password]
+      ); //inserting the registered usernames in the database
+      console.log(result);
+      res.render("secrets.ejs"); //rendering the ejs file after successfull registration
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/login", async (req, res) => {
   let email = req.body.username;
   let password = req.body.password;
+
+  const result=await db.query("SELECT * FROM users WHERE email=$1",[email]);  
+  if(result.rowCount>0){
+    const pass=result.rows[0].password;
+    if(pass==password){
+      res.render("secrets.ejs");
+    }
+    else{
+      res.json("Incorrect password");
+    }
+  }
+      res.json("Error while loggin in");
+
 });
 
 app.listen(port, () => {
