47c0bd97abe76f83a14178c82f4cf58475c0c899
Aryan Tambe <aryan.tambe04@gmail.com>
2025-06-06 22:40:54 +0530

    Postgres connection established

--- Authentication+Lv.1/index.js
+++ Authentication+Lv.1/index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "Aryan@2004",
+  port: 5432,
+});
+db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -20,17 +29,22 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-  let user=req.body.username;
-  console.log(user);
-  let pass=req.body.password;
-  console.log(pass);
-  res.send("Details saved");
+  let email = req.body.username;
+  let password = req.body.password;
+  const result = await db.query(
+    "INSERT INTO users (email,password) VALUES($1,$2)",
+    [email, password]
+  ); //inserting the registered usernames in the database
+  console.log(result);
+  res.render("secrets.ejs");
 });
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => {
+  let email = req.body.username;
+  let password = req.body.password;
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
   console.log(`http://localhost:${port}`);
-
 });
